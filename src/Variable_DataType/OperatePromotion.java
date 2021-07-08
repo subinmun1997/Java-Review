d78126d12a882a826f1881eb186f7922199ab236
subinmun1997 <qzxy812@gmail.com>
2021-07-08 23:31:51 +0900

    Variable/Data Type

---
+++ src/Variable_DataType/OperatePromotion.java
@@ -0,0 +1,12 @@
+package Variable_DataType;
+
+public class OperatePromotion {
+	
+	public static void main(String[] args) {
+		int num1 = 11;
+		int num2 = 22;
+		int result = num1 + num2;
+
+		System.out.println(result);
+	}
+}
