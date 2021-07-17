76b5cc8672754c9cde537f1f402fd9d38e1a0d0a
subinmun1997 <qzxy812@gmail.com>
2021-07-17 13:58:28 +0900

    Operators

---
+++ src/Operators/Q6.java
@@ -0,0 +1,12 @@
+package Operators;
+
+public class Q6 {
+	
+	public static void main(String[] args) {
+		int num = 7;
+		num = ~num;
+		num += 1;
+		System.out.println("num = " + num);
+		
+	}
+}
---
+++ src/Operators/Q7.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class Q7 {
+	
+	public static void main(String[] args) {
+		int num = 15678;
+		System.out.println((num >> 2) & 1);
+		System.out.println((num >> 4) & 1);
+	}
+}
