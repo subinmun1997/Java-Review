70fabf63c2deceb8953a52ae6b006b970e4f0c4e
subinmun1997 <qzxy812@gmail.com>
2021-07-25 12:35:25 +0900

    Class/Instance

---
+++ src/Class_Instance/BankAccountOO.java
@@ -0,0 +1,37 @@
+package Class_Instance;
+
+class BankAccount {
+	int balance = 0;
+	
+	public int deposit(int amount) {
+		balance += amount;
+		return balance;
+	}
+	
+	public int withdraw(int amount) {
+		balance -= amount;
+		return balance;
+	}
+	
+	public int checkMyBalance() {
+		System.out.println("ภÜพื: " + balance);
+		return balance;
+	}
+}
+
+public class BankAccountOO {
+	
+	public static void main(String[] args) {
+		BankAccount yoon = new BankAccount();
+		BankAccount park = new BankAccount();
+		
+		yoon.deposit(5000);
+		park.deposit(3000);
+		
+		yoon.withdraw(2000);
+		park.withdraw(2000);
+		
+		yoon.checkMyBalance();
+		park.checkMyBalance();
+	}
+}
